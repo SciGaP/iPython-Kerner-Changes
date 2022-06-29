559ec3a4fc1eeb88d43557a29b0784740b8d8b1f
Dinuka De Silva <l.dinukadesilva@gmail.com>
2022-06-29 13:47:13 -0400

    Style improvements

--- jupyter-platform/platform-ui/src/App.js
+++ jupyter-platform/platform-ui/src/App.js
@@ -1,13 +1,22 @@
 import './App.css';
 import MainComponent from './MainComponent';
+import logo from "./logo.svg"
 
 function App() {
-    
-  return (
-    <>
-    <MainComponent />
-    </>
-  );
+    return (
+        <div className="w-100">
+            <nav className="navbar navbar-dark bg-dark pl-3 pr-3">
+                <a className="navbar-brand" href="#">
+                    <img src={logo} width="30" height="30"
+                         className="d-inline-block align-top" alt=""/>
+                    Airavata Jupyter Platform
+                </a>
+            </nav>
+            <div className="w-100 p-3">
+                <MainComponent/>
+            </div>
+        </div>
+    );
 }
 
 export default App;
